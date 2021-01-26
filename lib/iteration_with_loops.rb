51525e6d8185a124dad5b8de4082e0e24e68ed0f
Mario Puerto <mariopuerto.nyc@gmail.com>
2021-01-26 19:33:57 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -1,15 +1,15 @@
 def find_even_values(src)
 
 row_index = 0
-while row_index < find_even_values.count do 
-  element_index = 0 
-  while element_index << find_even_values[row_index].count do 
+while row_index < find_even_values.count do
+  element_index = 0
+  while element_index << find_even_values[row_index].count do
 p find_even_values [row_index][element_index]
 element_index += 1
 
-   end 
+   end
   row_index += 1
-end 
+end
 
 
 
